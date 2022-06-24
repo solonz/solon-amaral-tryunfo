9245dfe87921ca6e16d3cefa20d69795d7274f39
Solon <amaral.solon@hotmail.com>
2022-06-24 18:30:46 -0300

    Requisito 03 :trophy:

---
+++ src/components/Card.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+  render() {
+    const {
+      cardName,
+      cardImage,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+    return (
+      <div>
+        <p data-testid="name-card">
+          {cardName}
+          {' '}
+        </p>
+        <img src={ cardImage } alt={ cardName } data-testid="image-card" />
+        <p data-testid="description-card">{cardDescription}</p>
+        <p data-testid="attr1-card">{cardAttr1}</p>
+        <p data-testid="attr2-card">{cardAttr2}</p>
+        <p data-testid="attr3-card">{cardAttr3}</p>
+        <p data-testid="rare-card">{cardRare}</p>
+        {cardTrunfo ? <p data-testid="trunfo-card">Super Trunfo</p> : ''}
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  cardName: PropTypes.string,
+  CardDescription: PropTypes.string,
+  cardAttr1: PropTypes.string,
+  cardAttr2: PropTypes.string,
+  cardAttr3: PropTypes.string,
+  cardImage: PropTypes.string,
+  cardRare: PropTypes.string,
+  cardTrunfo: PropTypes.bool,
+  hasTrunfo: PropTypes.bool,
+  isSaveButtonDisabled: PropTypes.bool,
+  onInputChange: PropTypes.func,
+  onSaveButtonClick: PropTypes.func,
+}.isRequired;
+
+export default Card;
